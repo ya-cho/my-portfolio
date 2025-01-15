293df8581c4529038200a08d0794ab9750f9e5a1
조윤아 <pj.yacho1@kalmate.net>
2025-01-15 09:10:44 +0900

    git 연동

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./assets/scss/global.scss";
+import { Header } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./../assets/scss/Header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>로고</h1>
+    </header>
+  );
+}
