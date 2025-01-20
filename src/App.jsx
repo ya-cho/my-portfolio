66957ae0c30885f18a651838f5149d480110d825
조윤아 <pj.yacho1@kalmate.net>
2025-01-20 17:57:13 +0900

    - 이미지명 수정, 주석 수정

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,15 @@ function App() {
     };
   }, []);
 
+  // app 렌더링 될 때 뷰포트 높이 계싼
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <>
       <div className="app">
