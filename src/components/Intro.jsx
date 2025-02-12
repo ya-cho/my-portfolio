d8216232692b7910178de96fcd98295c7c832639
조윤아 <pj.yacho1@kalmate.net>
2025-02-12 14:17:40 +0900

    인트로 수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -13,38 +13,48 @@ export default function Intro({ onComplete }) {
   const containerRef = useRef(null); // 전체 컨테이너
 
   useEffect(() => {
-    const text = textRef.current;
-    const obj = objRef.current;
+    // sessionStorage에서 'hasSeenIntro' 값을 체크하여 첫 방문 여부 확인
+    const hasSeenIntro = sessionStorage.getItem("hasSeenIntro");
 
-    if (text && obj) {
-      const distance = text.offsetWidth - obj.offsetWidth;
+    if (!hasSeenIntro) {
+      const text = textRef.current;
+      const obj = objRef.current;
 
-      const tween = gsap.to(obj, {
-        duration: 2,
-        x: distance,
-        rotation: 360,
-        ease: "none",
-        paused: true,
-      });
+      if (text && obj) {
+        const distance = text.offsetWidth - obj.offsetWidth;
 
-      tween.play();
+        const tween = gsap.to(obj, {
+          duration: 2,
+          x: distance,
+          rotation: 360,
+          ease: "none",
+          paused: true,
+        });
+
+        tween.play();
+      }
+
+      // fade out
+      const timer = setTimeout(() => {
+        gsap.to(containerRef.current, {
+          opacity: 0,
+          y: "-100vh",
+          duration: 1,
+          ease: "power2.out",
+          onComplete: () => {
+            if (onComplete) onComplete();
+            // 인트로가 끝난 후, 'hasSeenIntro' 값을 sessionStorage에 저장
+            sessionStorage.setItem("hasSeenIntro", "true");
+          },
+        });
+      }, 3000);
+
+      // clean
+      return () => clearTimeout(timer);
+    } else {
+      // 인트로 이미 재생한 경우 메인으로 바로 이동
+      if (onComplete) onComplete();
     }
-
-    // fade out
-    const timer = setTimeout(() => {
-      gsap.to(containerRef.current, {
-        opacity: 0,
-        y: "-100vh",
-        duration: 1,
-        ease: "power2.out",
-        onComplete: () => {
-          if (onComplete) onComplete();
-        },
-      });
-    }, 3000);
-
-    // clean
-    return () => clearTimeout(timer);
   }, [onComplete]);
 
   return (
